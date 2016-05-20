a8b8e2cdd95c4c66a7ed7348403de861278804c2
Andrey <andrey2111@mail.ru>
2016-05-21 02:49:38 +0300

    All tasks completed

--- city_game.py
+++ city_game.py
@@ -1,70 +1,53 @@
-from re import match
-import random
+from collections import defaultdict
 
-cities = {}
-used = set()
-current_char = ''
+cities = defaultdict(set)  # Словарь с городами: 'первая буква' : {set городов на эту букву}
+used = set()  # Множество уже названных городов
+current_char = ''  # Буква, с которой должен начинаться следующий город
 
-with open('ru_cities', encoding='UTF-8') as inf:
+with open('ru_cities', encoding='UTF-8') as inf:  # Чтение городов из файла
     for line in inf:
-        line = line.strip()
-        if cities.get(line[0]):
-            cities[line[0]].add(line)
-        else:
-            cities[line[0]] = {line}
+        cities[line[0]].add(line.rstrip())  # Добавление города в словарь
 
 print('Привет! Поиграем в города? Ты начинаешь! (Если надоест, просто введи: \'сдаюсь\')')
 print('Назови любой российский город:')
 while True:
-    if current_char and not cities[current_char]:
-        print('Ну и ну! Кажется, городов на эту букву не осталось, а значит, я выиграл!')
-        break
+    city = input().lower()  # Ввод города пользователем
 
-    city = input().lower()
-
-    # if current_char:
-    #     city = str(random.choice(list(cities[current_char])))
-    # else:
-    #     city = str(random.choice(list(cities['а'])))
-    # print(city.title())
-
-    if city == 'сдаюсь':
+    if city == 'сдаюсь':  # Условие выхода игрока
         print('Ура, я победил!')
         break
 
-    if current_char and not city.startswith(current_char):
+    if current_char and not city.startswith(current_char):  # Если город начинается не с той буквы
         print('Тебе нужно назвать город, начинающийся с \'{}\'! Попробуй ещё раз:'.format(current_char.upper()))
         continue
 
-    if not match(r'[а-я]+\-*[а-я]+', city):
-        print('Что-то я такого города не знаю... Попробуешь ещё раз?')
-        continue
-
-    if city in used:
+    if city in used:  # Проверка повторения городов
         print('Такой город уже был! Назови другой!')
         continue
 
-    if city in cities[city[0]]:
+    if city[0] not in cities or city not in cities[city[0]]:  # Если город не найден в списке, ввести повторно
+        print('Что-то я такого города не знаю... Попробуешь ещё раз?')
+    else:  # Город введён корректно
         used.add(city)
         cities[city[0]].remove(city)
-        if city[-1] in cities:
+        if city[-1] in cities:  # Последняя или предпоследняя буква города становится новой первой буквой
             current_char = city[-1]
         else:
             current_char = city[-2]
+        # Формирование ответа от компьютера:
         print('Отлично! Мне на \'{}\':'.format(current_char.upper()))
-        if not cities[current_char]:
-            print('Ну и ну! Кажется, городов на эту букву не осталось, а значит, я проиграл, поздравляю!')
+        if not cities[current_char]:  # Условие победы игрока
+            print('Ну и ну! Кажется, городов на букву \'{}\' не осталось, а значит, я проиграл, поздравляю!'.format(current_char.upper()))
             break
-        answer = cities[current_char].pop()
+        answer = cities[current_char].pop()  # Вабор ответа
         used.add(answer)
         print(answer.title())
-        if answer[-1] in cities:
+        if answer[-1] in cities:  # Ответ получен и задаётся новая первая буква
             current_char = answer[-1]
         else:
             current_char = answer[-2]
         print('Твоя очередь! Тебе на \'{}\':'.format(current_char.upper()))
-        continue
-    else:
-        print('Что-то я такого города не знаю... Попробуешь ещё раз?')
-        continue
 
+    if current_char and not cities[current_char]:  # Условие победы компьютера
+        print('Ну и ну! Кажется, городов на букву \'{}\' не осталось, а значит, я победил!'.format(current_char.upper()))
+        break
--- test_patron.py
+++ test_patron.py
@@ -3,10 +3,15 @@ import re
 
 names = []
 with open('names.txt', encoding='UTF-8') as inf:
-    for line in inf.readlines():
+    for line in inf:
         for i in re.findall(r'\b[А-Я]\w+\b', line):
             names.append(i)
 
+with open('names1.txt', encoding='UTF-8') as inf:
+    for line in inf:
+        if line.split()[0] != 'Русские':
+            names.append(line.split()[0])
+
 p = Patronymer()
 for name in names:
-    print(name + ' : ' + p.get_patro(name) + ', ' + p.get_patro(name, True))
+        print(name + ' : ' + p.get_patro(name) + ', ' + p.get_patro(name, True))
